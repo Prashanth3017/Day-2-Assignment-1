768eaa59c0c0dbdd31b683460d53a96e9981587e
Prashanth3017 <114842860+Prashanth3017@users.noreply.github.com>
2022-10-03 12:56:48 +0530

    Add files via upload
    Learned How to create Object
    How to call the global variables
    Access Modifiers
    Purpose of New keyword
    Datatypes

---
+++ TwoWheeler.java
@@ -0,0 +1,22 @@
+package week2.day1;
+
+public class TwoWheeler {
+	int noOfWheels = 2;
+	short noOfmirrors = 2;
+	long chasisNumbers = 32525410856314l;
+	boolean isPunctured = true;
+	String bikeName = "Hero Honda";
+	double runningKm = 894507.23;
+	public static void main(String[] args) {
+		TwoWheeler Bike = new TwoWheeler();
+		System.out.println(Bike.noOfWheels);
+		System.out.println(Bike.noOfmirrors);
+		System.out.println(Bike.chasisNumbers);
+		System.out.println(Bike.isPunctured);
+		System.out.println(Bike.bikeName);
+		System.out.println(Bike.runningKm);
+			// TODO Auto-generated method stub
+
+	}
+
+}
